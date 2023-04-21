a8700d33d5ed45e5780b491aaf2e13b422c75150
Entelodonto <jamesdanielporter@proton.me>
2023-04-21 16:23:16 +0100

    Error handling nonexistent url paths.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Reviews from "./components/Reviews";
 import Review from "./components/Review";
+import Notfound from "./components/Notfound";
 import { Navbar } from "./components/Navbar";
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
           <Route path="/" element={<Reviews />} />
           <Route path="/review/:id" element={<Review />} />
           <Route path="/reviews/:category" element={<Reviews />} />
+          <Route path="/*" element={<Notfound />} />
         </Routes>
       </Router>
     </div>
---
+++ src/components/Notfound.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Notfound() {
+  return <h1>Error, this url does not exist.</h1>;
+}
+
+export default Notfound;
